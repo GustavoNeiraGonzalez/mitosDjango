802d7468b892f274b8a4b759dd2dc77d42cb4bf4
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-03-08 02:47:07 -0300

    avanze en login, necesario mas investigación en el tema, para finalizar

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('MitosApp.urls')),
-    path('',include('LoginApp.urls'))
+    path('',include('LoginApp.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- Backend/LoginApp/views.py
+++ Backend/LoginApp/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from LoginApp.models import user 
 from LoginApp.serializers import loginSerializer
 # Create your views here.
+
+class loginViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = loginSerializer
+    queryset = user.objects.all()
 @csrf_exempt
 def loginapi(request,id=0):
     if request.method=='GET':
