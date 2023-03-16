7b8fc5e5a7afb4b835d2c1eb28acd871d498dfd7
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-03-16 19:22:43 -0300

    avanze crud mitos, ERROR AL MANDAR UN ARCHIVO DE IMAGEN

--- Backend/MitosApp/models.py
+++ Backend/MitosApp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class mitos(models.Model):
     mitoId = models.AutoField(primary_key=True)
     Mito = models.CharField(max_length=50)
-    Titulo = models.CharField(max_length=120)
-    Dioses = models.CharField(max_length=100)
-    Facciones = models.CharField(max_length=50)
+    Titulo = models.CharField(max_length=120, null=True)
+    Dioses = models.CharField(max_length=100, null=True)
+    Facciones = models.CharField(max_length=50, null=True)
     historia = models.CharField(max_length=1000)
     precio = models.IntegerField()
     Foto = models.ImageField(upload_to="ImagenMito", null=True)
--- client/src/pages/IngresarMitos/IngresarMitos.jsx
+++ client/src/pages/IngresarMitos/IngresarMitos.jsx
@@ -1,42 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './IngresarMitos.module.css'
+import axios from 'axios'
 export default function IngresarMitos() {
+    const [Mito,setMito] = useState('');
+    const [Historia,setHistoria] = useState('');
+    const [Precio,setPrecio] = useState('');
+    const [Foto,setFoto] = useState('');
+    const token = localStorage.getItem("token");
+    const tokenHeader = { Authorization: token };
 
+
+    const HandleClick = (e) => {
+        e.preventDefault()
+  
+      //useEffect para logear y guardar el token en el localstorage
+      if(Mito === '' || Historia==='' || Precio===0 ||isNaN(Precio)===true){
+        console.log("No dejar en blanco password username precio ")
+      }else{
+        axios.post("http://127.0.0.1:8000/api/mitos/",{
+            Mito:Mito,
+            historia:Historia,
+            precio:Precio,
+            Foto:Foto,
+            headers:tokenHeader
+        })
+        .then((response)=>{
+            console.log(response)
+        })
+        .catch(err => console.log(err.response.data))
+  
+      }
+    }
     
 
   return (
     <form className={style.form} >
         <div className={style.formgroup}>
         <label htmlFor="mito" className={style.formgrouplabel}>Mito</label>
-        <input
-            type="text"
-            name="mito"
+        <input type="text" name="mito"
             className={style.formgroupinput}
+            onChange={(e) => {
+                setMito(e.target.value);
+            }}
         />
         </div>
         <div className={style.formgroup}>
         <label htmlFor="historia" className={style.formgrouplabel}>Historia</label>
-        <input 
-            type="text"
-            name="historia"
+        <input type="text" name="historia"
             className={style.formgroupinput}
+            onChange={(e) => {
+                setHistoria(e.target.value);
+            }}
         />
         </div>
         <div className={style.formgroup}>
         <label htmlFor="precio" className={style.formgrouplabel}>Precio</label>
-        <input type="text"
-            name="precio"
+        <input type="text" name="precio"
             className={style.formgroupinput}
+            onChange={(e) => {
+                setPrecio(e.target.value);
+            }}
             />
         </div>
         <div className={style.formgroup}>
             <label htmlFor="foto" className={style.formgrouplabel}>Foto</label>
-            <input
-            type="file"
-            name="foto"
+            <input type="file" name="foto"
+            onChange={(e) => {
+                const archivo = e.target.files[0];
+                const archivoBase64 = archivo.toDataURL();
+                setFoto(archivoBase64);
+            }}
             />
         </div>
-        <button type="submit" className={style.button}>Enviar</button>
+        <button className={style.button} onClick={HandleClick}>Enviar</button>
     </form>
   )
 }
