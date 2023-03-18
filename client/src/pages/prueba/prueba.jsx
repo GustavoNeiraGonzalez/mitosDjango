7f49bdbf9f88f6deabaf62afc34ee479992954f3
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-03-18 18:38:44 -0300

    avanze carrito de compras :D

--- client/src/pages/prueba/prueba.jsx
+++ client/src/pages/prueba/prueba.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './prueba.module.css'
+export default function Prueba() {
+  
+  const products = [
+    { id: 1, name: 'Producto 1', price: 10 },
+    { id: 2, name: 'Producto 2', price: 20 },
+  ];
+  const [cartItems, setCartItems] = useState([]);
 
-export default function prueba() {
+  const handleAddToCart = (product) => {
+    setCartItems((prevItems) => [...prevItems, product]);
+  };
+
+  const handleRemoveFromCart = (product) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== product.id)
+    );
+  };
   return (
-    <div className={style.navbar}>
-      <a className={style.navbarlink} href='#home'>Home</a>
-      <a className={style.navbarlink} href='#about'>About</a>
-      <a className={style.navbarlink} href='#contact'>Contact</a>
-    </div>
+    <div className={style.color}>
+    <h2>Productos</h2>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} - ${product.price}
+          <button onClick={() => handleAddToCart(product)}>
+            Agregar al carrito
+          </button>
+        </li>
+      ))}
+    </ul>
+
+    <h2>Carrito de compras</h2>
+    <ul>
+      {cartItems.map((item) => (
+        <li key={item.id}>
+          {item.name} - ${item.price}
+          <button onClick={() => handleRemoveFromCart(item)}>
+            Eliminar del carrito
+          </button>
+        </li>
+      ))}
+    </ul>
+
+    <p>Total: ${cartItems.reduce((total, item) => total + item.price, 0)}</p>
+  </div>
   )
 }
